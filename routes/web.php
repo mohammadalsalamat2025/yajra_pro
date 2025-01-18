6ed8bbf893eb28781b6f178680f86e3e4cd43e98
Mohammad Alsalamat <mohammadalsalamat2025@gmail.com>
2025-01-18 20:23:04 +0300

    add code redirect to web.php file

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/employee');
+    #return view('welcome');
 });
+
 
 // Route::get('/employees', [EmployeeController::class, 'index'])->name('employee.index');
 Route::resource('employee', EmployeeController::class);
